acb7562357bc4377a9508d9a7785986bb1598f15
rsiew11 <rsiew11@gmail.com>
2022-12-11 17:05:34 -0500

    day2 part two start

--- day2/solve.py
+++ day2/solve.py
@@ -3,25 +3,45 @@ POINT_VALS = {
     "win": 6,
     "draw": 3,
     "lose": 0,
-    "A": 1,
-    "B": 2,
-    "C": 3,
-    "X": 1,
-    "Y": 2,
-    "Z": 3
-}
-
-WIN_CON = {
-    "A": ""
+    "A": 1, # rock
+    "B": 2, # paper
+    "C": 3, # scissor
+    "X": 1, # rock
+    "Y": 2, # paper
+    "Z": 3  # scissor
 }
 
 
-def main():
+def part1():
+    win_con = {
+        "X": "C",
+        "Y": "A",
+        "Z": "B"
+    }
+    draw_con = {
+        "X": "A",
+        "Y": "B",
+        "Z": "C"
+    }
     with open("input.txt","r") as fd:
-                # theirs, mine
         moves = [(line[0],line[-1]) for line in fd.read().splitlines()]
 
+    score = 0
+    for opp, me in moves:
+        score += POINT_VALS[me]
+        if win_con[me] == opp:
+            score += POINT_VALS["win"]
+        elif draw_con[me] == opp:
+            score += POINT_VALS["draw"]
+        else:
+            score += POINT_VALS["lose"]
+    return score
+
+def part2():
+    pass
 
 
 if __name__ == "__main__":
-    main()
+    print(part1())
+    print(part2())
+
